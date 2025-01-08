17663dacadbfac98efb60772832fba0d7d3a1d70
ABIHAAHMED4262 <abihaahmed413@gmail.com>
2025-01-08 22:46:27 +0500

    Changing in client.ts

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -3,8 +3,8 @@ import { createClient } from 'next-sanity'
 import { apiVersion, dataset, projectId } from '../env'
 
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
+  projectId:'o5t7im9q',
+  dataset:'production',
+  apiVersion:"v2025-01-07",
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
---
+++ src/sanity/schemaTypes/product.ts
@@ -0,0 +1,24 @@
+
+
+export const product={
+    name:'product',
+    type:'document',
+    title:'product',
+    fields:[
+        {
+        name:'title',
+        type:'string',
+        title:'Title'
+    } ,
+    {
+        name:'description',
+        type:'string',
+        title:'Description'
+    } ,
+    {
+        name:'image',
+        type:'image',
+        title:'Image'
+    } 
+]
+}
